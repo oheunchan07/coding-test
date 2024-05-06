a87754a2fa6b9f3f470fecc66aadef7bd1351b3b
오은찬 <131967057+oheunchan07@users.noreply.github.com>
2024-05-06 20:53:49 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+
+import java.io.IOException;
+
+import java.io.InputStreamReader;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {	
+        String s = "         ,r'\"7\n" + 	
+				   "r`-_   ,'  ,/\n" + 		
+				   " \\. \". L_r'\n" + 		
+				   "   `~\\/\n" + 			
+				   "      |\n" +
+                   "      |";
+
+ 
+
+		System.out.print(s);
+	}
+}
