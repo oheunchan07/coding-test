5d1f3439fbe2d8f36611827fc7534c0edaab3a56
오은찬 <131967057+oheunchan07@users.noreply.github.com>
2024-05-15 13:38:36 +0900

    [Silver III] Title: N과 M (3), Time: 580 ms, Memory: 157556 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int[] arr;
+    static int n;
+    static int m;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        dfs(0);
+        bw.flush();
+
+    }
+
+    static void dfs(int d) throws IOException {
+        if(d == m) {
+            for(int i = 0;i<m;i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+
+        for(int i = 0;i<n;i++) {
+                arr[d] = i+1;
+                dfs(d+1);
+        }
+    }
+}
