11d9e60860956e989e8037e78be9b00ab9a0533e
오은찬 <131967057+oheunchan07@users.noreply.github.com>
2024-05-15 16:32:05 +0900

    [Silver III] Title: N과 M (5), Time: 2684 ms, Memory: 81496 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int[] arr;
+    static int[] arrN;
+    static boolean[] visit;
+    static int n;
+    static int m;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        arrN = new int[n];
+        visit = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0;i<n;i++)
+            arrN[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arrN);
+
+        dfs(0);
+
+    }
+
+    static void dfs(int d) {
+        if(d == m) {
+            for(int i = 0;i<m;i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0;i<n;i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                arr[d] = arrN[i];
+                dfs(d+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
