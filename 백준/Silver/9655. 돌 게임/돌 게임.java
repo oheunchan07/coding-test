4ed24af378373e3ef1d394361df1fea635436ea3
오은찬 <131967057+oheunchan07@users.noreply.github.com>
2024-05-16 11:06:34 +0900

    [Silver V] Title: 돌 게임, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if(n%2 == 1)
+            System.out.println("SK");
+        else
+            System.out.println("CY");
+    }
+}
