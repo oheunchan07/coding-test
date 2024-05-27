649e7e6c6bc59ea77fc85727a064cfb0bf974300
오은찬 <131967057+oheunchan07@users.noreply.github.com>
2024-05-27 15:11:17 +0900

    [Gold IV] Title: 음식 평론가, Time: 124 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int a = m;
+        while (a > 0) {
+            int temp = n;
+            n = a;
+            a = temp % a;
+        }
+
+        System.out.println(m - n);
+    }
+}
